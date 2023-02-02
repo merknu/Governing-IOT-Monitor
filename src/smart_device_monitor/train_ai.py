64a4b12e00c1da6e705b9c2840992a79c88cf74c
merknu <8459791+merknu@users.noreply.github.com>
2023-02-02 15:24:40 +0100

    Update train_ai.py

--- src/smart_device_monitor/train_ai.py
+++ src/smart_device_monitor/train_ai.py
@@ -1,26 +1,18 @@
-# train_ai.py
-import torch
-import tensorflow as tf
-from sklearn import svm
-from keras.models import Sequential
-from keras.layers import Dense
+import os
+import importlib
 
-def train_ai(library, model_params, training_data, labels):
-    if library == "PyTorch":
-        # PyTorch code here to train AI using the given model_params and training_data
-        pass
-    elif library == "TensorFlow":
-        # TensorFlow code here to train AI using the given model_params and training_data
-        pass
-    elif library == "scikit-learn":
-        # scikit-learn code here to train AI using the given model_params and training_data
-        clf = svm.SVC(**model_params)
-        clf.fit(training_data, labels)
-    elif library == "Keras":
-        # Keras code here to train AI using the given model_params and training_data
-        model = Sequential()
-        model.add(Dense(**model_params))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        model.fit(training_data, labels, epochs=10, batch_size=32)
-    else:
-        raise Exception("Invalid library specified. Choose from PyTorch, TensorFlow, scikit-learn, and Keras.")
+MODEL_FOLDER = 'models'
+
+def train_ai(data, labels):
+    models = []
+    
+    # List all the models in the MODEL_FOLDER
+    for filename in os.listdir(MODEL_FOLDER):
+        if filename.endswith('.py'):
+            models.append(filename[:-3])
+
+    # Import the models dynamically
+    for model_name in models:
+        module = importlib.import_module(MODEL_FOLDER + '.' + model_name)
+        model = module.create_model()
+        model.fit(data, labels)
