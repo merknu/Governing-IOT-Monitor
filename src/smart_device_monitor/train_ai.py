b938e67a50aa407a6cb5a0e09e4b1c1785b375cd
merknu <8459791+merknu@users.noreply.github.com>
2023-02-02 15:28:27 +0100

    Update train_ai.py

--- src/smart_device_monitor/train_ai.py
+++ src/smart_device_monitor/train_ai.py
@@ -1,18 +1,9 @@
 import os
-import importlib
 
-MODEL_FOLDER = 'models'
-
-def train_ai(data, labels):
-    models = []
-    
-    # List all the models in the MODEL_FOLDER
-    for filename in os.listdir(MODEL_FOLDER):
-        if filename.endswith('.py'):
-            models.append(filename[:-3])
-
-    # Import the models dynamically
-    for model_name in models:
-        module = importlib.import_module(MODEL_FOLDER + '.' + model_name)
-        model = module.create_model()
-        model.fit(data, labels)
+def find_model_folders():
+    model_folder = 'data/model_files/'
+    model_folders = []
+    for subdir, dirs, files in os.walk(model_folder):
+        for dir in dirs:
+            model_folders.append(subdir + '/' + dir)
+    return model_folders
